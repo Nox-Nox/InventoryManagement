83a912b73725467646dbd2525ac0d6be48bee60e
Nur Tahmid <noxana31@gmail.com>
2022-04-23 03:16:07 +0200

    Implemented DBConnect class and created function to connect to DB, created AppController to implement and in App.java theres code for insert into DB for temporary tests use

--- app/src/main/java/InventoryManagement/App.java
+++ app/src/main/java/InventoryManagement/App.java
@@ -9,19 +9,27 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class App extends Application {
 
-    public static void main(String[] args) {
-       launch();
-    }
+   @Override
+   public void start(Stage primaryStage) throws Exception {
+      DBConnect co = new DBConnect();
+      String query = "insert into product values(4, 'SVR45YR666HR', 'papera', 100, 3.5)";
+      Statement stat = co.connectToDB().createStatement();
+      stat.execute(query);
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-       Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
-       Scene scene = new Scene(root);
-       primaryStage.setTitle("Inventory Management");
-       primaryStage.setScene(scene);
-       primaryStage.show();
-        
-    }
+      Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+      Scene scene = new Scene(root);
+      primaryStage.setTitle("Inventory Management");
+      primaryStage.setScene(scene);
+      primaryStage.show();
+
+   }
+
+   public static void main(String[] args) {
+      launch();
+   }
 }
---
+++ app/src/main/java/InventoryManagement/DBConnect.java
@@ -0,0 +1,22 @@
+package InventoryManagement;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnect {
+    public Connection conn;
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        String connArg = "jdbc:h2:/home/tahmid/Documents/Projects/InventoryManagement/app/src/main/H2DB/H2;SCHEMA=ITEMS";
+        Class.forName("org.h2.Driver");
+        conn = DriverManager.getConnection(connArg, "root", "dianawhore99");
+        return conn;
+    }
+
+    public Connection connectToDB() throws ClassNotFoundException, SQLException {
+        DBConnect dbconnect = new DBConnect();
+        return dbconnect.getConnection();
+    }
+
+}
